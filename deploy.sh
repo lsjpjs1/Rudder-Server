f12cdbfa0c6b208b41271dc69d90e552eacff042
lsjpjs1 <lsjpjs1@naver.com>
2021-07-26 21:43:19 +0900

    chore : 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd /home/rudder-docker
 
-sudo docker rm rudder-server
+
 sudo docker build -t rudder-server -f Dockerfile .
-sudo docker run --name rudder-server -p 3000:3000 rudder-server
+sudo docker run --name rudder-server --env-file ./env.list -p 3000:3000 rudder-server
