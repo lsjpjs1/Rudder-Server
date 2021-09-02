6a98e4588e6c0cfef86b661df9d544967dc3639b
박성훈 <lsjpjs1@naver.com>
2021-09-02 21:38:38 +0900

    fix : renderPost 쿼리 에러 수정

--- routes/board.js
+++ routes/board.js
@@ -52,7 +52,7 @@ async function renderPost(board_type,endPostId,category_id=0,user_id,school_id){
                 var results = await client.query(baseQuery+"and b.post_id >= (select post_id from (select post_id from board where is_delete = false and post_id<$3 and school_id=$5 order by post_id desc limit $4  ) as not_delete order by post_id asc limit 1) \
                 and b.school_id=$5 and b.post_id < $3 order by post_id desc",[user_id,board_type,endPostId,POST_NUMBER_IN_ONE_PAGE,school_id])
             }else{
-                var results = await client.query(baseQuery+"and b.post_id >= (select post_id from (select post_id from board where is_delete = false and post_id<$3 and category_id=$4 and school_id=$6 order by post_id desc limit $4  ) as not_delete order by post_id asc limit 1) \
+                var results = await client.query(baseQuery+"and b.post_id >= (select post_id from (select post_id from board where is_delete = false and post_id<$3 and category_id=$4 and school_id=$6 order by post_id desc limit $5  ) as not_delete order by post_id asc limit 1) \
                 and b.school_id=$6 and b.post_id < $3 and b.category_id=$4 order by post_id desc",[user_id,board_type,endPostId,category_id,POST_NUMBER_IN_ONE_PAGE,school_id])
             }
             
