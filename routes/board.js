641fcc3aef1110cac44a815fef06d86cf00a5791
박성훈 <lsjpjs1@naver.com>
2021-09-02 20:48:18 +0900

    header check test

--- routes/board.js
+++ routes/board.js
@@ -484,6 +484,7 @@ router.post("/renderPost",async function(req,res){
     
     const {board_type,endPostId,category_id,token} = req.body; 
     console.log(board_type,endPostId)
+    console.log(req.headers)
     
     if(tk.decodeToken(token)){
         const tmp = jwt.verify(token,SECRET_KEY)
