48a9d5eea465d25e82c4efdf76d5a12600891c2c
박성훈 <lsjpjs1@naver.com>
2021-09-13 19:51:27 +0900

    fix : 로그 수정

--- routes/schoolverify.js
+++ routes/schoolverify.js
@@ -134,8 +134,6 @@ console.log(email)
         }else{
             await client.query("insert into email_verification values (default,$1, $2)",[email,authNum])
             await client.query("COMMIT")
-        
-            console.log(process.env.GOOGLE_USER,process.env.GOOGLE_PASS)
             const mailOptions = {
                 from: process.env.GOOGLE_USER,
                 to: email,
