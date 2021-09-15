3d7694b904dcea66ef1044c80dee1484001fbf72
박성훈 <lsjpjs1@naver.com>
2021-09-15 18:11:52 +0900

    hotfix

--- routes/comment.js
+++ routes/comment.js
@@ -73,8 +73,7 @@ async function addComment(user_id,post_id, comment_body,status,group_num){
             from board_comment where post_id = $1 group by group_num) as c",[post_id])
             await client.query("insert into board_comment values (default, $1, $2, $3, default, 0,$4,0,$5)",[post_id,user_id,comment_body,status,queryResult.rows[0].count])
         }else{
-            queryResult = await client.query("select count(c.count), \
-            from (SELECT count(comment_id) from board_comment where post_id = $1 and group_num = $2 group by order_in_group) as c",[post_id,group_num])
+            queryResult = await client.query("select count(c.count) from (SELECT count(comment_id) from board_comment where post_id = $1 and group_num = $2 group by order_in_group) as c",[post_id,group_num])
             await client.query("insert into board_comment values (default, $1, $2, $3, default, 0,$4,$5,$6)",[post_id,user_id,comment_body,status,queryResult.rows[0].count,group_num])
         }
         os = queryResult.rows[0].os
