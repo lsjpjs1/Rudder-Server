cecf343c8a628efb98c43df4b7b00988ec3976ab
박성훈 <lsjpjs1@naver.com>
2021-09-18 09:43:43 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-# cd /home/rudder-docker
+cd /home/rudder-docker
 
-# sudo docker rm rudder-server -f
-# sudo docker build -t rudder-server -f Dockerfile .
-# sudo docker run --name rudder-server --env-file ./env.list -p 3001:3000 rudder-server
+sudo docker rm rudder-server -f
+sudo docker build -t rudder-server -f Dockerfile .
+sudo docker run --name rudder-server --env-file ./env.list -p 3001:3000 rudder-server
