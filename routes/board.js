63e3c18ca6be5ae63c24a659585b28499bb8726c
박성훈 <lsjpjs1@naver.com>
2021-09-20 14:18:04 +0900

    test

--- routes/board.js
+++ routes/board.js
@@ -27,9 +27,7 @@ const { profile } = require('console');
 
 router.post("/categoryList",async function(req,res){
     var {school_id,token} = req.body
-    console.log('schoolid',req.body.school_id)
-    console.log('token',req.body.token)
-    if(typeof token != 'undefined' && token !=null){
+    if(typeof token != 'undefined'){
         const tmp = jwt.verify(token,SECRET_KEY)
         school_id=tmp.school_id
     }
@@ -46,7 +44,7 @@ async function categoryList(school_id=1){
             category = result
             categoryList.push(category)
         }
-        return tescategoryList
+        return categoryList
     }catch(ex){
         console.log("Failed to execute categoryList"+ex)
         await client.query("ROLLBACK")
