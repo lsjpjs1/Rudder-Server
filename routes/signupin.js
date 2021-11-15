b587ef8277a23ac64d8808ccd8ace51a7bc65e75
lsjpjs1 <lsjpjs1@naver.com>
2021-11-15 16:43:30 +0900

    fix : ios version upgrade

--- routes/signupin.js
+++ routes/signupin.js
@@ -334,7 +334,7 @@ async function getNotice(os,version) {
         if(os=='android'&&version!=ANDROID_VERSION){
             NOTICE_BODY=UPDATE_BODY
         }
-        if(os=='ios'&&version!='3.0.5'){
+        if(os=='ios'&&version!='3.0.6'){
             NOTICE_BODY=UPDATE_BODY
         }
         return {isExist:IS_EXIST,notice:NOTICE_BODY}
