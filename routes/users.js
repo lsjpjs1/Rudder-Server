d8a807a2e4df6d045a6ca373546dc85e8ef28c11
lsjpjs1 <lsjpjs1@naver.com>
2021-12-17 15:32:24 +0900

    feat : blockUser api 추가

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,38 @@ async function addUserRequest(user_info_id,body){
   }
 }
 
+
 router.post("/addUserRequest",async function(req,res){
 
   const {token,request_body} = req.body
   if(tk.decodeToken(token)){
     const tmp = jwt.verify(token,SECRET_KEY)
     await addUserRequest(tmp.user_info_id,request_body).then(res.send(JSON.stringify({results:{isSuccess:true}})))
+  }
+});
+
+
+async function blockUser(user_info_id,blockUserId){
+  try{
+    await client.query("BEGIN")
+    await client.query("insert into user_block values (default,$1,(select user_info_id from user_info where user_id = $2))",[user_info_id,blockUserId])
+    await client.query("COMMIT")
+  }catch(ex){
+      console.log("Failed to execute blockUser"+ex)
+      await client.query("ROLLBACK")
+  }finally{
+     // await client.end()
+      console.log("Cleaned.") 
+  }
+}
+
+router.post("/blockUser",async function(req,res){
+
+  const {token,blockUserId} = req.body
+  if(tk.decodeToken(token)){
+    const tmp = jwt.verify(token,SECRET_KEY)
+    await blockUser(tmp.user_info_id,blockUserId)
+    res.send(JSON.stringify({results:{isSuccess:true}}))
   }
 });
 
