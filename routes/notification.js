bf4ca14016592ccac4544cfc00eef121b7d998e0
lsjpjs1 <lsjpjs1@naver.com>
2022-02-14 14:06:00 +0900

    fix : 알림 로그 수정

--- routes/notification.js
+++ routes/notification.js
@@ -44,13 +44,8 @@ module.exports.notificationFromToken = async function (os, notification_token,bo
             note.topic = process.env.IOS_APP_BUNDLE_NAME;
             // 실제 메시지를 보내도록 합니다.
             apn_provider.send(note, deviceToken).then(function (result) {
-                if (typeof result.failed[0].response == "undefined") {
-                    console.log("결과 : " + result.failed[0].response.reason);
-                    console.log(result);
-                } else {
-                    console.log("Apn notification success")
-                }
-                
+                console.log("결과 : " + result.failed[0].response.reason);
+                console.log(result);
             }).catch(function (err) {
                 throw (err);
             });
