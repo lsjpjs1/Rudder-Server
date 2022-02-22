f45298be0a625d526268b256d4da1f5ed60342e8
lsjpjs1 <lsjpjs1@naver.com>
2022-02-22 19:58:37 +0900

    fix : 주소 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/rudderSite.html');
+  res.writeHead(301, {
+    Location: 'https://bush-thorn-7ed.notion.site/Rudder-05a628bb03a44097ada829222aec0603'
+  }).end();
 });
 
 module.exports = router;
