1b8d43830923a2a9b2975fdc35f52b54720604ae
lsjpjs1 <lsjpjs1@naver.com>
2022-02-23 14:44:49 +0900

    fix : 자기 자신한테 메시지, 댓글 알림 안가게

--- routes/comment.js
+++ routes/comment.js
@@ -65,7 +65,7 @@ async function commentRender(post_id, user_id){
     }
 }
 
-async function addComment(user_id,post_id, comment_body,status,group_num){
+async function addComment(user_id,post_id, comment_body,status,group_num,user_info_id){
     try{
         await client.query("BEGIN")
         var queryResult
@@ -74,6 +74,7 @@ async function addComment(user_id,post_id, comment_body,status,group_num){
         var insertResult
         var commentTime
         var notificationType
+        var flag = false
         if(status=="parent"){
             queryResult = await client.query("select count(c.count), \
             (select notification_token from user_info where user_id = (select user_id from board where post_id = $1 )), \
@@ -81,7 +82,11 @@ async function addComment(user_id,post_id, comment_body,status,group_num){
             (select os from user_info where user_id = (select user_id from board where post_id = $1 )) from (SELECT count(comment_id) \
             from board_comment where post_id = $1 group by group_num) as c",[post_id])
             insertResult = await client.query("insert into board_comment values (default, $1, $2, $3, default, 0,$4,0,$5) returning *",[post_id,user_id,comment_body,status,queryResult.rows[0].count])
-            await notification.saveNotificationInfo(1,queryResult.rows[0].user_info_id,insertResult.rows[0].comment_id)
+            if (user_info_id != queryResult.rows[0].user_info_id){
+                flag = true
+                await notification.saveNotificationInfo(1,queryResult.rows[0].user_info_id,insertResult.rows[0].comment_id)
+            }
+            
             notificationType = 1
         }else{
             queryResult= await client.query("\
@@ -91,14 +96,21 @@ async function addComment(user_id,post_id, comment_body,status,group_num){
             (select user_info_id from user_info where user_id = (select user_id from board_comment where post_id = $1 and group_num=$2 and order_in_group=0)) \
             from (SELECT count(comment_id) from board_comment where post_id = $1 and group_num = $2 group by order_in_group) as c",[post_id,group_num])
             insertResult =await client.query("insert into board_comment values (default, $1, $2, $3, default, 0,$4,$5,$6) returning *",[post_id,user_id,comment_body,status,queryResult.rows[0].count,group_num])
-            await notification.saveNotificationInfo(3,queryResult.rows[0].user_info_id,insertResult.rows[0].comment_id)
+            if (user_info_id != queryResult.rows[0].user_info_id){
+                flag = true
+                await notification.saveNotificationInfo(3,queryResult.rows[0].user_info_id,insertResult.rows[0].comment_id)
+            }
+            
             notificationType = 3
         }
         os = queryResult.rows[0].os
         notification_token = queryResult.rows[0].notification_token
         console.log(queryResult.rows[0])
         
-        await notification.notificationFromToken(os,notification_token,comment_body,notificationType) // undefined check는 notificationFromToken에서 함
+        if (flag){
+            await notification.notificationFromToken(os,notification_token,comment_body,notificationType) // undefined check는 notificationFromToken에서 함 
+        }
+        
         await client.query("update board set comment_count = comment_count+1 where post_id=($1)",[post_id])
         await client.query("COMMIT")
         
@@ -291,7 +303,7 @@ router.post("/addComment",async function(req,res){
 
     if(tk.decodeToken(token)){
         var temp = jwt.verify(token,SECRET_KEY)
-        await addComment(temp.user_id,post_id, comment_body,status,group_num).then(res.send(JSON.stringify({results:{isSuccess:true}})))
+        await addComment(temp.user_id,post_id, comment_body,status,group_num,temp.user_info_id).then(res.send(JSON.stringify({results:{isSuccess:true}})))
         userRecord.insertUserActivity(temp.user_info_id,"comment")
         
     }else{
